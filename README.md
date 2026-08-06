059a786f7c02f1c4858aa0f1fd488a071c30684a
Priyanshu <emailatpkumar@gmail.com>
2026-08-06 11:49:13 +0530

    created-project-skeleton-

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # dbms-project
-so it's the first one .//
-making changes from vs code
--- dbms-engine1.py
+++ dbms-engine1.py
@@ -1,1 +1,106 @@
+#insert function
+#only the logic since this file doesn't hold the data
 
+
+
+
+
+
+
+
+
+
+
+
+#delete function
+#only the logic
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#search function
+
+
+
+
+
+
+
+
+
+
+
+
+
+#find min funciton
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#find max function
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#get_size funciton
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#get height function
+
+
+
+
+
+
+
+
+
+
+
+
+
+#clear function
+#it resets a bst to none
